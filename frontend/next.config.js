e52cfddd7d458926c2c2c659cfcb85850ea4ff8f
amirgithuber <amirmaleki3993@gmail.com>
2025-10-19 10:03:16 +0330

    Add standalone output for Docker production build

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
+  // این خط کلیدی است و به Next.js میگوید یک خروجی مستقل برای داکر بسازد
+  output: 'standalone',
 }
 
 module.exports = nextConfig
